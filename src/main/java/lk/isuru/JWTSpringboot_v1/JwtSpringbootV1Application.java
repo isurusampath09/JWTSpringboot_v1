0235b692963e6e5f6d4894086346548a6f3a8a0c
Isuru Sampath <isurusampathcs@gmail.com>
2023-10-12 12:50:04 +0530

    Config jwt spring boot project

---
+++ src/main/java/lk/isuru/JWTSpringboot_v1/JwtSpringbootV1Application.java
@@ -0,0 +1,13 @@
+package lk.isuru.JWTSpringboot_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringbootV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringbootV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/lk/isuru/JWTSpringboot_v1/JwtSpringbootV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.isuru.JWTSpringboot_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringbootV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
